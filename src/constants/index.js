6108e56590d27bac2a5101bd966239f34f86bb26
Miftahussalam <miftah_salam@ymail.com>
2022-08-25 07:20:22 +0700

    alamat maps

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@
  * Link
  */
 export const GOOGLE_CALENDAR_LINK = `https://calendar.google.com/calendar/event?action=TEMPLATE&dates=20220911T010000Z%2F20220911T080000Z&text=Arin+%26+Miftah+Wedding&details=Arin+%26+Miftah+Wedding`;
-export const GOOGLE_MAPS_LINK = `https://g.page/HARRISHotelSentralandSemarang?share`;
+export const GOOGLE_MAPS_LINK = `https://goo.gl/maps/kfFTBJKwRhw3qWut8`;
 
 /**
  * Wedding time
