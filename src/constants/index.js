330fabe2ba12e9e26edf93f1de6ab90af1447379
thang <thangnh2@vega.com.vn>
2023-01-12 16:46:25 +0700

    change

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 /**
  * Link
  */
-export const GOOGLE_CALENDAR_LINK = `https://calendar.google.com/calendar/event?action=TEMPLATE&dates=20220911T010000Z%2F20220911T080000Z&text=Arin+%26+Miftah+Wedding&details=Arin+%26+Miftah+Wedding`;
+export const GOOGLE_CALENDAR_LINK = `https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NjkyMGV0bXZ2MjJkdnE4ajAwMnY3Z2drbTQgdGhhbmduaDJAdmVnYS5jb20udm4&tmsrc=thangnh2%40vega.com.vn`;
 export const GOOGLE_MAPS_LINK = `https://goo.gl/maps/kfFTBJKwRhw3qWut8`;
 
 /**
@@ -9,4 +9,4 @@ export const GOOGLE_MAPS_LINK = `https://goo.gl/maps/kfFTBJKwRhw3qWut8`;
  */
 export const EPOCH_START_EVENT = 1662858000;
 export const EPOCH_END_EVENT = 1662858000;
-export const UTC_WEDDING_TIME = '2022-09-11:04:00:00Z';
+export const UTC_WEDDING_TIME = '2023-03-12:08:00:00Z';
